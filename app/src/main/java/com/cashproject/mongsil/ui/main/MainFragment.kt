45cafd3044aba4b958666095b9947955b3e38eb6
Haeseong5 <dhgotjd1234@naver.com>
2021-04-03 01:30:56 +0900

    뒤로가기 클릭 시 광고 다이어로그 노출

--- app/src/main/java/com/cashproject/mongsil/ui/main/MainFragment.kt
+++ app/src/main/java/com/cashproject/mongsil/ui/main/MainFragment.kt
@@ -1,21 +1,22 @@
 package com.cashproject.mongsil.ui.main
 
 import android.content.Context
+import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.cashproject.mongsil.R
 import com.cashproject.mongsil.base.BaseFragment
 import com.cashproject.mongsil.databinding.FragmentSplashBinding
-import com.cashproject.mongsil.extension.showToast
 import com.cashproject.mongsil.ui.pages.calendar.CalendarFragment
 import com.cashproject.mongsil.ui.pages.home.HomeFragment
 import com.cashproject.mongsil.ui.pages.locker.LockerFragment
 import com.cashproject.mongsil.viewmodel.CalendarViewModel
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.subjects.PublishSubject
-import java.util.concurrent.TimeUnit
+import gun0912.ted.tedadmobdialog.OnBackPressListener
+import gun0912.ted.tedadmobdialog.TedAdmobDialog
 
 class MainFragment : BaseFragment<FragmentSplashBinding, CalendarViewModel>() {
 
@@ -25,10 +26,12 @@ class MainFragment : BaseFragment<FragmentSplashBinding, CalendarViewModel>() {
     override val viewModel: CalendarViewModel by viewModels {viewModelFactory}
 
     private lateinit var callback: OnBackPressedCallback
+    private lateinit var nativeTedAdmobDialog: TedAdmobDialog
 
-    private val backBtnSubject = PublishSubject.create<Boolean>() // backBtn 이벤트를 발생시킬 수 있는 Subject
-    private val BACK_BTN_EXIT_TIMEOUT = 2000 // 연속된 Back 버튼의 시간 간격 (2초안에 백버튼 2번 클릭시 앱 종료)
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initAdmobDialog()
+    }
     override fun initStartView() {
         val fragments = arrayOf<Fragment>(
             CalendarFragment(),
@@ -52,31 +55,13 @@ class MainFragment : BaseFragment<FragmentSplashBinding, CalendarViewModel>() {
     override fun onResume() {
 //        viewModel.getAllComments() //여기서 데이터 요청하는데, 캘린더에서 옵저브가 안됨,,
         super.onResume()
-        addDisposable(backBtnSubject
-            .debounce(100, TimeUnit.MILLISECONDS)
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnNext {
-                Log.v(TAG, "backBtnSubject | onNext")
-            }
-            .timeInterval(TimeUnit.MILLISECONDS)
-            .skip(1)
-            .filter { interval ->
-                Log.v(TAG, "backBtnSubject | interval: $interval")
-                interval.time() < BACK_BTN_EXIT_TIMEOUT
-            }
-            .subscribe {
-//                finishActivity(0)
-//                finish()
-                requireActivity().finish()
-            })
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                backBtnSubject.onNext(true)
-                activity?.showToast("뒤로가기를 한번 더 누르면 앱이 종료됩니다.")
+                showAdmobDialog()
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(this, callback)
@@ -84,5 +69,35 @@ class MainFragment : BaseFragment<FragmentSplashBinding, CalendarViewModel>() {
     override fun onDetach() {
         super.onDetach()
         callback.remove()
+    }
+
+    private fun showAdmobDialog(){
+        nativeTedAdmobDialog.show()
+    }
+
+    /**
+     * https://github.com/ParkSangGwon/TedAdmobDialog
+     */
+    private fun initAdmobDialog(){
+        nativeTedAdmobDialog =
+            TedAdmobDialog.Builder(requireActivity(), TedAdmobDialog.AdType.NATIVE, "ca-app-pub-3940256099942544/2247696110")
+                .showReviewButton(true)
+                .setOnBackPressListener(object : OnBackPressListener {
+                    override fun onReviewClick() {
+                        Log.d(TAG, "onReviewClick")
+                    }
+                    override fun onFinish() {
+                        Log.d(TAG, "onFinish")
+                        requireActivity().finish()
+                    }
+
+                    override fun onAdShow() {
+                        Log.d(TAG, "onAdShow")
+                        nativeTedAdmobDialog.loadNative()
+                    }
+                })
+                .create()
+
+        nativeTedAdmobDialog.loadNative()
     }
 }
