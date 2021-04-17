380c6a03a65f923b4d2355a0b77d70be87a94eb2
haeseong-oh <haeseong.oh@naverz-corp.com>
2021-04-17 20:22:44 +0900

    color 설정

--- app/src/main/java/com/cashproject/mongsil/ui/pages/setting/SettingFragment.kt
+++ app/src/main/java/com/cashproject/mongsil/ui/pages/setting/SettingFragment.kt
@@ -6,35 +6,14 @@ import android.content.Context.ALARM_SERVICE
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
-import com.cashproject.mongsil.R
-import com.cashproject.mongsil.base.BaseFragment
 import com.cashproject.mongsil.extension.showToast
 import com.cashproject.mongsil.receiver.AlarmReceiver
 import com.cashproject.mongsil.util.PreferencesManager
 import java.util.*
-import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
-import com.cashproject.mongsil.databinding.FragmentLockerBinding
 import com.cashproject.mongsil.databinding.FragmentSettingBinding
-import com.cashproject.mongsil.extension.addTo
-import com.cashproject.mongsil.model.data.Saying
 import com.cashproject.mongsil.ui.dialog.DiaryListBottomSheetFragment
-import com.cashproject.mongsil.ui.main.MainFragment
-import com.cashproject.mongsil.ui.pages.locker.LockerAdapter
-import com.cashproject.mongsil.util.FragmentListener
-import com.cashproject.mongsil.util.RxEventBus
-import com.cashproject.mongsil.viewmodel.LockerViewModel
-import kotlinx.coroutines.launch
-
-import kotlin.collections.ArrayList
 
 
 class SettingFragment : Fragment(){
@@ -64,16 +43,16 @@ class SettingFragment : Fragment(){
     }
 
     fun showBottomListDialog() {
-        val bottomSheetDiaryListFragment = DiaryListBottomSheetFragment()
-        bottomSheetDiaryListFragment.show(childFragmentManager, "approval")
-        bottomSheetDiaryListFragment.setCheckBtnOnClickListener { hour, minute ->
+        val bottomSheetAlarmDialog = DiaryListBottomSheetFragment()
+        bottomSheetAlarmDialog.show(childFragmentManager, "approval")
+        bottomSheetAlarmDialog.setCheckBtnOnClickListener { hour, minute ->
             PreferencesManager.hour = hour
             PreferencesManager.minute = minute
             setAlarm(hour, minute)
             activity?.showToast("매일 ${hour}시 ${minute}분에 푸시메세지가 전송됩니다!")
-            bottomSheetDiaryListFragment.dismiss()
+            bottomSheetAlarmDialog.dismiss()
         }
-        bottomSheetDiaryListFragment.setReleaseBtnClickListener {
+        bottomSheetAlarmDialog.setReleaseBtnClickListener {
             //알람해제
             val intent = Intent(activity, AlarmReceiver::class.java)  // 1. 알람 조건이 충족되었을 때, 리시버로 전달될 인텐트를 설정합니다.
             PendingIntent.getBroadcast(     // 2 PendingIntent가 이미 존재할 경우 cancel 하고 다시 생성
