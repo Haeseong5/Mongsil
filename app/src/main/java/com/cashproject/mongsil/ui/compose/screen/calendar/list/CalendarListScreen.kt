e594eccaa614bcaf38e46daae99572b6a39884bf
Haeseong5 <dhgotjd1234@naver.com>
2023-12-20 23:32:29 +0900

    정사각형 이미지 대화면에서 짤리는 이슈 수정

--- app/src/main/java/com/cashproject/mongsil/ui/compose/screen/calendar/list/CalendarListScreen.kt
+++ app/src/main/java/com/cashproject/mongsil/ui/compose/screen/calendar/list/CalendarListScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
@@ -63,6 +64,7 @@ fun CalendarDay(
 ) {
     Box {
         AsyncImage(
+            modifier = Modifier.aspectRatio(1f, true),
             model = poster.squareImage,
             contentScale = ContentScale.Crop,
             contentDescription = ""
