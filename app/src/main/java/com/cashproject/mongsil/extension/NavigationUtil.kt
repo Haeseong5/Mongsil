ecd35103d5c044e586f5cf1efcde5dc4e7a356bd
Haeseong5 <dhgotjd1234@naver.com>
2023-12-25 05:33:10 +0900

    시스템 Ui 풀스크린 적용

---
+++ app/src/main/java/com/cashproject/mongsil/extension/NavigationUtil.kt
@@ -0,0 +1,51 @@
+package com.cashproject.mongsil.extension
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Point
+import android.graphics.Rect
+import android.os.Build
+import android.view.WindowInsets
+import android.view.WindowManager
+
+fun Context.getNavigationBarHeight(): Int {
+    val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+    return if (Build.VERSION.SDK_INT >= 30) {
+        windowManager
+            .currentWindowMetrics
+            .windowInsets
+            .getInsets(WindowInsets.Type.navigationBars())
+            .bottom
+
+    } else {
+        val currentDisplay = try {
+            display
+        } catch (e: NoSuchMethodError) {
+            windowManager.defaultDisplay
+        }
+
+        val appUsableSize = Point()
+        val realScreenSize = Point()
+        currentDisplay?.apply {
+            getSize(appUsableSize)
+            getRealSize(realScreenSize)
+        }
+
+        // navigation bar on the side
+        if (appUsableSize.x < realScreenSize.x) {
+            return realScreenSize.x - appUsableSize.x
+        }
+
+        // navigation bar at the bottom
+        return if (appUsableSize.y < realScreenSize.y) {
+            realScreenSize.y - appUsableSize.y
+        } else 0
+    }
+}
+
+fun getStatusBarHeight(activity: Activity): Int {
+    val rectangle = Rect()
+    activity.window.decorView.getWindowVisibleDisplayFrame(rectangle)
+    return rectangle.top
+}
--- app/src/main/java/com/cashproject/mongsil/ui/MainActivity.kt
+++ app/src/main/java/com/cashproject/mongsil/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.cashproject.mongsil.ui
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.lifecycleScope
 import com.cashproject.mongsil.R
 import com.cashproject.mongsil.base.BaseActivity
@@ -10,6 +12,7 @@ import com.cashproject.mongsil.databinding.ActivityMainBinding
 import com.cashproject.mongsil.di.Injection
 import com.cashproject.mongsil.extension.makeStatusBarTransparent
 import com.cashproject.mongsil.data.firebase.fcm.PushManager
+import com.cashproject.mongsil.extension.getNavigationBarHeight
 import com.cashproject.mongsil.ui.dialog.ProgressDialog
 import com.cashproject.mongsil.ui.main.MainViewModel
 import com.cashproject.mongsil.viewmodel.ViewModelFactory
@@ -42,8 +45,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     private val pushManager = PushManager()
     private val remoteConfigManager by lazy { RemoteConfigManager(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        this.makeStatusBarTransparent()
+        binding.root.setPadding(0, 0, 0, getNavigationBarHeight())
         MobileAds.initialize(this)
         setupPushNotification()
         remoteConfigManager.apply {
