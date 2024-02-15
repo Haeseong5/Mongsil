8b2a539a91372d63cf1a6b3e2b7a5c5f2d595cc2
gkrls <gkrlsanjf@naver.com>
2024-02-15 22:32:18 +0900

    다이어리 상단 탭 이모티콘 클릭 이벤트 추가
    - 상단에 있는 이모티콘과 날짜를 클릭하면 이모티콘을 선택할 수 있는 바텀시트가 뜹니다.
    
    모듈화 작업을 진행하기 위해 커밋함.

--- app/src/main/java/com/cashproject/mongsil/ui/pages/diary/DiaryScreen.kt
+++ app/src/main/java/com/cashproject/mongsil/ui/pages/diary/DiaryScreen.kt
@@ -2,17 +2,29 @@ package com.cashproject.mongsil.ui.pages.diary
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+
 
 @Composable
 fun DiaryScreen(
     diaryViewModel: DiaryViewModel,
     onUiEvent: (DiaryUiEvent) -> Unit = {}
 ) {
-    val uiState = diaryViewModel.uiState.collectAsState()
+    val uiState by diaryViewModel.uiState.collectAsState()
     val sideEffect = diaryViewModel.sideEffect
+
     DiaryScreenContent(
-        uiState = uiState.value,
+        uiState = uiState,
         sideEffect = sideEffect,
         onUiEvent = onUiEvent
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewDiaryScreen() {
+    DiaryScreenContent(
+        uiState = DiaryUiState(),
     )
 }
