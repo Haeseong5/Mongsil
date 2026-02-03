e43857a6f4fb285aa1dc2d89f8c5cc1d9348d66f
yunseong <gkrlsanjf@naver.com>
2026-02-03 16:38:52 +0900

    chore: add coil-kmp dependencies to build configuration

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -92,6 +92,10 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.ktor.client.logging)
+            
+            // Coil - Image Loading (KMP)
+            implementation(libs.coil.compose.kmp)
+            implementation(libs.coil.network.ktor)
         }
     }
 }
