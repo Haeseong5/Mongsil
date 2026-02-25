9cda71a34ab5fe92b70869cd1af31f42c5d6e662
yunseong <gkrlsanjf@naver.com>
2026-02-25 16:08:48 +0900

    fix: apply consumeWindowInsets to DiaryWriteScreenContent
    - add consumeWindowInsets modifier with padding to handle insets properly
    - ensure UI adjusts correctly for system window insets in DiaryWriteScreen

--- composeApp/src/commonMain/kotlin/com/cashproject/mongsil/kmp/screen/diarywrite/DiaryWriteScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cashproject/mongsil/kmp/screen/diarywrite/DiaryWriteScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -83,7 +84,7 @@ fun DiaryWriteScreen(
     }
 
     DiaryWriteScreenContent(
-        modifier = modifier.padding(padding),
+        modifier = modifier.padding(padding).consumeWindowInsets(padding),
         uiState = uiState,
         onEvent = viewModel::onEvent
     )
