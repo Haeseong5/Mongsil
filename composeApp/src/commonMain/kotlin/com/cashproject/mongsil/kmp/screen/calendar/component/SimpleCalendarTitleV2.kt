b0a5566f1242d10ebe19c3d7ae608846429ab8ab
haeseong5 <dhgotjd1234@naver.com>
2026-03-08 14:56:32 +0900

    타이틀 폰트 사이즈 수정

--- composeApp/src/commonMain/kotlin/com/cashproject/mongsil/kmp/screen/calendar/component/SimpleCalendarTitleV2.kt
+++ composeApp/src/commonMain/kotlin/com/cashproject/mongsil/kmp/screen/calendar/component/SimpleCalendarTitleV2.kt
@@ -1,20 +1,16 @@
 package com.cashproject.mongsil.kmp.screen.calendar.component
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.cashproject.mongsil.kmp.designsystem.Gray600
+import com.cashproject.mongsil.kmp.designsystem.Gray800
 import com.cashproject.mongsil.kmp.designsystem.MongsilTheme
 import com.cashproject.mongsil.kmp.designsystem.component.HorizontalSpacer
 import mongsil.composeapp.generated.resources.Res
@@ -35,15 +31,15 @@ fun SimpleCalendarTitleV2(
             modifier = Modifier,
             text = "${year}년 ${month}월",
             textAlign = TextAlign.Center,
-            style = MongsilTheme.typography.title3,
+            style = MongsilTheme.typography.title2,
         )
         HorizontalSpacer(8.dp)
         Icon(
             modifier = Modifier
-                .size(14.dp),
+                .size(20.dp),
             painter = painterResource(Res.drawable.ic_baseline_arrow_forward_ios_24),
             contentDescription = "forward",
-            tint = Gray600
+            tint = Gray800
         )
     }
 }
